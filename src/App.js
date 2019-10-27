b755b073fc40c09835742e64a934e001afe1d09a
muhammadsarim555 <muhammadsarim555@gmail.com>
2019-10-27 11:23:40 -0700

    Todo Funcnality Add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [todos, setTodos] = useState([]);
+  const [value, setValue] = useState("")
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    const newTodos = [...todos, {text: value}];
+    console.log(newTodos, ">>")
+    setTodos(newTodos);
+    setValue("");
+  };
+
+return(
+  <div>
+    <form onSubmit={handleSubmit}>
+    <input
+      type="text"
+      className="input"
+      value={value}
+      onChange={e => setValue(e.target.value)}
+    />
+    {/* <button onClick={handleSubmit}>submit</button> */}
+  </form>
+    {
+      todos.map((v,i) => <div key={i}>{v.text}</div>)
+    }
+<p>this is salman</p>
+  </div>
+)
+}
 export default App;
